b7ba95bf03eb8eecf9c1befbb5a2857668e4d718
324a2119 <st324a2119k.matsuda@urayama.ac.jp>
2025-10-09 11:43:18 +0900

    user.java

---
+++ user.java
@@ -0,0 +1,21 @@
+package com.example.cocosuku.model;
+
+public class User {
+    private int userId;
+    private String email;
+    private String passwordHash;
+    private String name;
+
+    // --- getter & setter ---
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
